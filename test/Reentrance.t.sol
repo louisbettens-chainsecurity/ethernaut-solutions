aca625fa7277e5a2e167fed9a75a12fa5596f5a5
Louis Bettens <louis@bettens.info>
2023-07-19 21:50:52 +0200

    solve with polygl0ts

---
+++ test/Reentrance.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+import "../src/Reentrance.sol";
+import "../src/Exploit.sol";
+
+contract ReentranceTest is Test {
+    Reentrance public reentrance;
+    Exploit public exploit;
+
+    function setUp() public {
+        reentrance = new Reentrance();
+	payable(reentrance).transfer(0.01 ether);
+    }
+
+    function testExploit() public {
+        exploit = new Exploit{value: 0.001 ether}(reentrance);
+	exploit.yoink();
+	assert(address(reentrance).balance == 0);
+    }
+}
